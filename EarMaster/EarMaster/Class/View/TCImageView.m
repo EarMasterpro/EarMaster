ceeb51c678d9b60d991a7488352760a0c5540f37
xuyida <xu94da@sina.com>
2015-09-04 20:18:48 +0800

    xyd: VIEW

---
+++ EarMaster/EarMaster/Class/View/TCImageView.m
@@ -0,0 +1,21 @@
+//
+//  TCImageView.m
+//  EarMaster
+//
+//  Created by 徐艺达 on 15/9/4.
+//  Copyright (c) 2015年 EM. All rights reserved.
+//
+
+#import "TCImageView.h"
+
+@implementation TCImageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

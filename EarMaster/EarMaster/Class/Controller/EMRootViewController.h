c99449886ff6143ea8a9a1aa06f0a7fa75b4fcce
xuyida <xu94da@sina.com>
2015-09-04 18:14:48 +0800

    xyd: first push obj

--- EarMaster/EarMaster/Class/Controller/EMRootViewController.h
+++ EarMaster/EarMaster/Class/Controller/EMRootViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  EMRootViewController.h
 //  EarMaster
 //
 //  Created by 徐艺达 on 15/9/4.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface EMRootViewController : UIViewController
 
 @end
-
---
+++ EarMaster/EarMaster/Class/Controller/EMRootViewController.m
@@ -0,0 +1,37 @@
+//
+//  EMRootViewController.m
+//  EarMaster
+//
+//  Created by 徐艺达 on 15/9/4.
+//  Copyright (c) 2015年 EM. All rights reserved.
+//
+
+#import "EMRootViewController.h"
+
+@interface EMRootViewController ()
+
+@end
+
+@implementation EMRootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
